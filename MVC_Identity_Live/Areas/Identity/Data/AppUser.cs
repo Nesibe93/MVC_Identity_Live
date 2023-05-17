74ba13061a831c1d2f4830b0d97a22493ebfd0da
Nesibe93 <125722258+Nesibe93@users.noreply.github.com>
2023-05-17 13:54:16 +0300

    Add project files.

---
+++ MVC_Identity_Live/Areas/Identity/Data/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MVC_Identity_Live.Areas.Identity.Data
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; } // Yeni Alan
+
+        public int PhoneNumber { get; set; }
+    }
+}
---
+++ MVC_Identity_Live/Areas/Identity/Data/MVC_Identity_LiveContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MVC_Identity_Live.Areas.Identity.Data;
+
+namespace MVC_Identity_Live.Data;
+
+public class MVC_Identity_LiveContext : IdentityDbContext<IdentityUser>
+{
+    public MVC_Identity_LiveContext(DbContextOptions<MVC_Identity_LiveContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+
+        //Burada AppUser tarafına tanımlanmış alanları varolan yapıya ekleme/güncelleme işlemleri yapılıyor
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+    }
+}
+
+
+public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<AppUser>
+{
+    public void Configure(EntityTypeBuilder<AppUser> builder)
+    {
+        // burada yeni eklemelerimi belirtiyorum
+
+        builder.Property(u => u.FirstName).HasMaxLength(20);
+        builder.Property(u => u.LastName).HasMaxLength(20);
+        builder.Property(u => u.City).HasMaxLength(50);
+    }
+}
